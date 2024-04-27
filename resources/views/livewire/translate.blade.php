1b7e4bc8cc996ce1d74ff23bdf0db9b5c87c8bb8
jhdcruz <jhdcruz@proton.me>
2024-04-27 19:02:17 +0800

    perf: lazy-load lang codes

--- resources/views/livewire/translate.blade.php
+++ resources/views/livewire/translate.blade.php
@@ -20,7 +20,7 @@
                 <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                     Translate from
                 </label>
-                <select wire:model="from_lang" id="countries"
+                <select wire:model.lazy="from_lang" id="countries"
                         class="max-w-fit mb-4 p-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option selected>Choose a language</option>
 
@@ -67,7 +67,7 @@
                 </label>
                 <select id="countries"
                         class="max-w-fit mb-4 p-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        wire:model="to_lang">
+                        wire:model.lazy="to_lang">
                     <option selected>Choose a language</option>
 
                     {{-- List available languages support by ACS --}}
